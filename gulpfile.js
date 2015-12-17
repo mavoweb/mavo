0e5a0290e2aa65cea51c7c291e065c29d96b9faa
Lea Verou <lea@verou.me>
2015-12-16 21:19:26 -0500

    Ditch CodeKit and use Gulp <3 instead

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+/*
+Build file to concat & minify files, compile SCSS and so on.
+npm install gulp gulp-util gulp-uglify gulp-rename gulp-concat gulp-sass --save-dev
+*/
+// grab our gulp packages
+var gulp  = require('gulp');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename')
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+
+gulp.task('concat', function() {
+	var files = "stretchy wysie storage unit scope primitive collection storage.dropbox ".split(" ").map(path => "components/" + path + ".js");
+
+	return gulp.src(files)
+		.pipe(concat('wysie.js'))
+		.pipe(gulp.dest('.'));
+});
+
+gulp.task('sass', function() {
+	return gulp.src(["**/*.scss", "!node_modules/**"])
+		.pipe(sass().on('error', sass.logError))
+		.pipe(rename({ extname: '.css' }))
+		.pipe(gulp.dest('.'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(["*.js, *.scss"], ['concat', 'sass']);
+});
+
+gulp.task('default', ['concat', 'sass']);
--- wysie.js
+++ wysie.js
@@ -1,144 +1,3 @@
-(function ($, $$) {
-
-var _ = self.Wysie = $.Class({
-	constructor: function (element) {
-		_.all.push(this);
-
-		var me = this;
-
-		// Assign a unique (for the page) id to this wysie instance
-		this.id = element.id || "wysie-" + _.all.length;
-		
-		// TODO escaping of # and \
-		var dataStore = element.getAttribute("data-store");
-		this.store = dataStore === "none"? null : new URL(dataStore || this.id, location);
-
-		this.element = _.is("scope", element)? element : $(_.selectors.scope, element);
-
-		if (!this.element) {
-			element.setAttribute("typeof", "");
-			this.element = element;
-		}
-
-		this.wrapper = element !== this.element? element : document.createElement("div")._.around(this.element);
-
-		this.wrapper.classList.add("wysie-root");
-
-		element.removeAttribute("data-store");
-
-		// Apply heuristic for collections
-		$$("li:only-of-type, tr:only-of-type", this.wrapper).forEach(element=>{
-			if (_.is("property", element) || _.is("scope", element)) {
-				element.setAttribute("data-multiple", "");
-			}
-		});
-
-		// Build wysie objects
-		this.root = new (_.is("multiple", this.element)? _.Collection : _.Scope)(this.element, this);
-		
-		// Fetch existing data
-		if (this.store && this.store.href) {
-			this.storage = _.Storage.create(this);
-
-			this.storage.load();
-		}
-	},
-
-	get data() {
-		return this.getData();
-	},
-
-	getData: function(o) {
-		return this.root.getData(o);
-	},
-
-	toJSON: function() {
-		return JSON.stringify(this.data, null, "\t");
-	},
-
-	render: function(data) {
-		this.root.render(data.data || data);
-	},
-
-	save: function() {
-		this.storage && this.storage.save();
-	},
-
-	static: {
-		all: [],
-
-		// Convert an identifier to readable text that can be used as a label
-		readable: function (identifier) {
-			// Is it camelCase?
-			return identifier && identifier
-			         .replace(/([a-z])([A-Z][a-z])/g, function($0, $1, $2) { return $1 + " " + $2.toLowerCase()}) // camelCase?
-			         .replace(/([a-z])[_\/-](?=[a-z])/g, "$1 ") // Hyphen-separated / Underscore_separated?
-			         .replace(/^[a-z]/, function($0) { return $0.toUpperCase() }); // Capitalize
-		},
-
-		// Inverse of _.readable(): Take a readable string and turn it into an identifier
-		identifier: function (readable) {
-			return readable && readable
-			         .replace(/\s+/g, "-") // Convert whitespace to hyphens
-			         .replace(/[^\w-]/g, "") // Remove weird characters
-			         .toLowerCase();
-		},
-
-		queryJSON: function(data, path) {
-			if (!path || !data) {
-				return data;
-			}
-
-			path = path.split("/");
-
-			for (var i=0, p; p=path[i++];) {
-				if (!data) {
-					return null;
-				}
-
-				data = data[p];
-			}
-
-			return data;
-		},
-
-		selectors: {
-			property: "[property], [itemprop]",
-			primitive: "[property]:not([typeof]), [itemprop]:not([itemscope])",
-			scope: "[typeof], [itemscope], [itemtype], .scope",
-			multiple: "[multiple], [data-multiple], .multiple",
-			required: "[required], [data-required], .required",
-			formControl: "input, select, textarea",
-			computed: ".computed" // Properties or scopes with computed properties, will not be saved
-		},
-
-		is: function(thing, element) {
-			return element.matches(_.selectors[thing]);
-		}
-	}
-});
-
-$.ready().then(evt=>{
-	$$("[data-store]").forEach(function (element) {
-		new Wysie(element);
-	});
-});
-
-})(Bliss, Bliss.$);
-
-// TODO implement this properly
-function safeval(expr, vars) {
-	with (vars) {
-		return eval(expr);
-	}
-}
-
-if (self.Promise && !Promise.prototype.done) {
-	Promise.prototype.done = function(callback) {
-		return this.then(callback, callback);
-	};
-}
-
 /*
  * Stretchy: Form element autosizing, the way it should be.
  * by Lea Verou http://lea.verou.me 
@@ -326,7 +185,146 @@ if (self.MutationObserver) {
 }
 
 })();
+(function ($, $$) {
 
+var _ = self.Wysie = $.Class({
+	constructor: function (element) {
+		_.all.push(this);
+
+		var me = this;
+
+		// Assign a unique (for the page) id to this wysie instance
+		this.id = element.id || "wysie-" + _.all.length;
+		
+		// TODO escaping of # and \
+		var dataStore = element.getAttribute("data-store");
+		this.store = dataStore === "none"? null : new URL(dataStore || this.id, location);
+
+		this.element = _.is("scope", element)? element : $(_.selectors.scope, element);
+
+		if (!this.element) {
+			element.setAttribute("typeof", "");
+			this.element = element;
+		}
+
+		this.wrapper = element !== this.element? element : document.createElement("div")._.around(this.element);
+
+		this.wrapper.classList.add("wysie-root");
+
+		element.removeAttribute("data-store");
+
+		// Apply heuristic for collections
+		$$("li:only-of-type, tr:only-of-type", this.wrapper).forEach(element=>{
+			if (_.is("property", element) || _.is("scope", element)) {
+				element.setAttribute("data-multiple", "");
+			}
+		});
+
+		// Build wysie objects
+		this.root = new (_.is("multiple", this.element)? _.Collection : _.Scope)(this.element, this);
+		
+		// Fetch existing data
+		if (this.store && this.store.href) {
+			this.storage = _.Storage.create(this);
+
+			this.storage.load();
+		}
+	},
+
+	get data() {
+		return this.getData();
+	},
+
+	getData: function(o) {
+		return this.root.getData(o);
+	},
+
+	toJSON: function() {
+		return JSON.stringify(this.data, null, "\t");
+	},
+
+	render: function(data) {
+		this.root.render(data.data || data);
+	},
+
+	save: function() {
+		this.storage && this.storage.save();
+	},
+
+	static: {
+		all: [],
+
+		// Convert an identifier to readable text that can be used as a label
+		readable: function (identifier) {
+			// Is it camelCase?
+			return identifier && identifier
+			         .replace(/([a-z])([A-Z][a-z])/g, function($0, $1, $2) { return $1 + " " + $2.toLowerCase()}) // camelCase?
+			         .replace(/([a-z])[_\/-](?=[a-z])/g, "$1 ") // Hyphen-separated / Underscore_separated?
+			         .replace(/^[a-z]/, function($0) { return $0.toUpperCase() }); // Capitalize
+		},
+
+		// Inverse of _.readable(): Take a readable string and turn it into an identifier
+		identifier: function (readable) {
+			return readable && readable
+			         .replace(/\s+/g, "-") // Convert whitespace to hyphens
+			         .replace(/[^\w-]/g, "") // Remove weird characters
+			         .toLowerCase();
+		},
+
+		queryJSON: function(data, path) {
+			if (!path || !data) {
+				return data;
+			}
+
+			path = path.split("/");
+
+			for (var i=0, p; p=path[i++];) {
+				if (!data) {
+					return null;
+				}
+
+				data = data[p];
+			}
+
+			return data;
+		},
+
+		selectors: {
+			property: "[property], [itemprop]",
+			primitive: "[property]:not([typeof]), [itemprop]:not([itemscope])",
+			scope: "[typeof], [itemscope], [itemtype], .scope",
+			multiple: "[multiple], [data-multiple], .multiple",
+			required: "[required], [data-required], .required",
+			formControl: "input, select, textarea",
+			computed: ".computed" // Properties or scopes with computed properties, will not be saved
+		},
+
+		is: function(thing, element) {
+			return element.matches(_.selectors[thing]);
+		}
+	}
+});
+
+$.ready().then(evt=>{
+	$$("[data-store]").forEach(function (element) {
+		new Wysie(element);
+	});
+});
+
+})(Bliss, Bliss.$);
+
+// TODO implement this properly
+function safeval(expr, vars) {
+	with (vars) {
+		return eval(expr);
+	}
+}
+
+if (self.Promise && !Promise.prototype.done) {
+	Promise.prototype.done = function(callback) {
+		return this.then(callback, callback);
+	};
+}
 (function($){
 
 var _ = Wysie.Storage = $.Class({ abstract: true,
@@ -610,7 +608,6 @@ _.Default = $.Class({ extends: _,
 });
 
 })(Bliss);
-
 /*
  * Wysie Unit: Super class that Scope and Primitive inherit from
  */
@@ -684,7 +681,6 @@ var _ = Wysie.Unit = $.Class({ abstract: true,
 });
 
 })(Bliss, Bliss.$);
-
 (function($, $$){
 
 var _ = Wysie.Scope = $.Class({
@@ -1007,7 +1003,6 @@ var _ = Wysie.Scope = $.Class({
 });
 
 })(Bliss, Bliss.$);
-
 (function($, $$){
 
 var _ = Wysie.Primitive = $.Class({
@@ -1421,7 +1416,6 @@ _.types = {
 };
 
 })(Bliss, Bliss.$);
-
 (function($, $$){
 
 var _ = Wysie.Collection = function (template, wysie) {
@@ -1634,7 +1628,6 @@ _.prototype = {
 };
 
 })(Bliss, Bliss.$);
-
 (function($){
 
 if (!self.Wysie) {
@@ -1737,4 +1730,3 @@ var _ = Wysie.Storage.Dropbox = $.Class({ extends: Wysie.Storage,
 });
 
 })(Bliss);
-
