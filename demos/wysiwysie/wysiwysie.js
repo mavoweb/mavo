7edf3a495ead3e6096c3619d4f744a80afd90957
Lea Verou <lea@verou.me>
2016-06-17 04:33:23 +0200

    more wysiwysie

--- demos/wysiwysie/wysiwysie.js
+++ demos/wysiwysie/wysiwysie.js
@@ -1,13 +1,17 @@
 tinymce.init({
 	selector: "textarea",
-	toolbar: "styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table | wysieproperty wysiemultiple",
-	extended_valid_elements: "@[data-store|property|data-multiple]",
+	toolbar1: "styleselect | bold italic | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent",
+	toolbar2: "image link | table | wysieproperty wysiemultiple",
+	plugins: "image code link table charmap colorpicker lists media tabfocus textcolor",
+	//menubar: "file edit insert view format table tools",
+	//extended_valid_elements: "*[data-store|property|itemprop|data-multiple]",
+	valid_elements: "*[*]",
 	setup: function (editor) {
 		editor.addButton("wysieproperty", {
 			text: "Wysie property",
 			icon: false,
 			onclick: function () {
-				editor.selection.getNode().setAttribute("property", prompt("Editable"));
+				editor.selection.getNode().setAttribute("property", prompt("Property name?", "thing"));
 			}
 		});
 
@@ -34,6 +38,6 @@ tinymce.init({
 		});
 	},
 	content_css: [
-		//"../../wysie.css"
+		"editor.css"
 	]
 });
