74b3bd7def84565def8806eb97d5be860c804ed8
Lea Verou <lea@verou.me>
2016-06-17 04:40:18 +0200

    wysiwysie sorta works!

--- demos/wysiwysie/wysiwysie.js
+++ demos/wysiwysie/wysiwysie.js
@@ -1,11 +1,12 @@
 tinymce.init({
 	selector: "textarea",
 	toolbar1: "styleselect | bold italic | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent",
-	toolbar2: "image link | table | wysieproperty wysiemultiple",
-	plugins: "image code link table charmap colorpicker lists media tabfocus textcolor",
+	toolbar2: "image link | table | visualblocks wysieproperty wysiemultiple",
+	plugins: "visualblocks image code link table charmap colorpicker lists media tabfocus textcolor",
 	//menubar: "file edit insert view format table tools",
 	//extended_valid_elements: "*[data-store|property|itemprop|data-multiple]",
 	valid_elements: "*[*]",
+	visualblocks_default_state: true,
 	setup: function (editor) {
 		editor.addButton("wysieproperty", {
 			text: "Wysie property",
@@ -24,14 +25,14 @@ tinymce.init({
 		});
 
 		editor.on("setAttrib input init", evt => {
-			console.log(editor.getContent());
+			console.log(editor.getContent({format: 'raw'}));
 			result.setAttribute("srcdoc", `<!DOCTYPE html>
 <html lang="en">
 <head><meta charset="UTF-8" />
 <title>Wysie</title>
 <link rel="stylesheet" href="../../wysie.css" />
 </head>
-<body data-store="local">${editor.getContent()}
+<body data-store="local">${editor.getContent({format: 'raw'})}
 <script src="../../wysie.js"></script>
 </body>
 </html>`);
