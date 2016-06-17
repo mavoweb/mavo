bc9e8ef84096c5a83fe6b66cbd2b3715a7dafac7
Lea Verou <lea@verou.me>
2016-06-17 12:05:31 +0200

    [wysiwysie] Disable buttons if not applicable

--- demos/wysiwysie/wysiwysie.js
+++ demos/wysiwysie/wysiwysie.js
@@ -12,30 +12,63 @@ tinymce.init({
 			text: "Wysie app",
 			icon: false,
 			onclick: function () {
-				editor.selection.getNode().setAttribute("data-store", prompt("Where to store data?", "local"));
+				var node = editor.selection.getNode();
+				node = node.closest("[data-store]") || node.closest("body > *") || node;
+				node.setAttribute("data-store", prompt("Where to store data?", "local"));
+
 				update();
+
+				nodeChange(editor.selection.getNode());
 			}
 		});
 
+		var propertyButton;
 		editor.addButton("wysieproperty", {
 			text: "Property",
 			icon: false,
+			disabled: true,
 			onclick: function () {
 				editor.selection.getNode().setAttribute("property", prompt("Property name?", "thing"));
 				update();
-			}
+			},
+			onPostRender : function() { propertyButton = this; }
 		});
 
+		function nodeChange(element) {
+			requestAnimationFrame(() => {
+				var wysie = element.matches("[data-store], [data-store] *");
+console.log(element, element.children > 0 ,!element.hasAttribute("data-multiple"));
+				propertyButton && propertyButton.disabled(!wysie || element.children.length > 0);
+
+				multipleButton && multipleButton.disabled(!wysie);
+			})
+
+		}
+
+		editor.on("NodeChange", function(event) {
+			nodeChange(event.element);
+		});
+
+		var multipleButton;
 		editor.addButton("wysiemultiple", {
 			text: "Multiple",
 			icon: false,
+			disabled: true,
 			onclick: function () {
-				editor.selection.getNode().setAttribute("data-multiple", "");
+				var node = editor.selection.getNode();
+				node.setAttribute("data-multiple", "");
+
+				if (!node.hasAttribute("property")) {
+					node.setAttribute("property", prompt("Property name?", "thing"));
+				}
+
 				update();
-			}
+			},
+			onPostRender : function() { multipleButton = this; }
 		});
 
 		function update() {
+			result.removeAttribute("srcdoc");
 			result.setAttribute("srcdoc", `<!DOCTYPE html>
 <html lang="en">
 <head><meta charset="UTF-8" />
