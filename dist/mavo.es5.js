mavoweb/mavo

commit 848298e5707e6472966e1cd38b7a094c739bba49

Fixed bug related to adding items to collections with deleted items
If last expression item (or first in bottom-up collections) was deleted
and the deletion notice was cleared, adding new items via the add
button threw an error.

dist/mavo.es5.js
@@ -3468,9 +3468,7 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 
 			if (this.mutable) {
 				// Add it to the DOM, or fix its place
-				var nextItem = this.children[index];
-
-				item.element._.before(nextItem && nextItem.element || this.marker);
+				$[this.bottomUp ? "after" : "before"](item.element, this.marker);
 			}
 
 			var env = { context: this, item: item };
@@ -3648,18 +3646,10 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 			if (this.mutable) {
 				// Insert the add button if it's not already in the DOM
 				if (!this.addButton.parentNode) {
-					if (this.bottomUp) {
-						this.addButton._.before($.value(this.children[0], "element") || this.marker);
-					} else {
-						var tag = this.element.tagName.toLowerCase();
-						var containerSelector = Mavo.selectors.container[tag];
-
-						if (containerSelector) {
-							var after = this.marker.parentNode.closest(containerSelector);
-						}
-
-						this.addButton._.after(after && after.parentNode ? after : this.marker);
-					}
+					var tag = this.element.tagName.toLowerCase();
+					var containerSelector = Mavo.selectors.container[tag];
+					var rel = containerSelector ? this.marker.parentNode.closest(containerSelector) : this.marker;
+					$[this.bottomUp ? "before" : "after"](this.addButton, rel);
 				}
 
 				// Set up drag & drop
@@ -3841,7 +3831,7 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 					Mavo.hooks.run("collection-add-end", env);
 				});
 
-				this.marker.parentNode.insertBefore(fragment, this.marker);
+				$[this.bottomUp ? "after" : "before"](fragment, this.marker);
 			}
 		},
 
@@ -4646,6 +4636,7 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 			if (this.primitive) {
 				this.primitive.value = ret;
 			} else {
+				ret = ret.presentational || ret;
 				Mavo.Primitive.setValue(this.node, ret, { attribute: this.attribute });
 			}
 
@@ -4796,7 +4787,7 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 			rootGroup.walk(function (obj, path) {
 				if (obj.expressions && obj.expressions.length && !obj.isDeleted()) {
 					var env = { context: _this2, data: $.value.apply($, [data].concat(_toConsumableArray(path))) };
-
+					// if (evt && evt.action == "delete") console.log(data, path, env.data, obj.element);
 					Mavo.hooks.run("expressions-update-start", env);
 
 					var _iteratorNormalCompletion2 = true;

dist/mavo.js
@@ -3081,9 +3081,7 @@ var _ = Mavo.Collection = $.Class({
 
 		if (this.mutable) {
 			// Add it to the DOM, or fix its place
-			var nextItem = this.children[index];
-
-			item.element._.before(nextItem && nextItem.element || this.marker);
+			$[this.bottomUp? "after" : "before"](item.element, this.marker);
 		}
 
 		var env = {context: this, item};
@@ -3204,19 +3202,10 @@ var _ = Mavo.Collection = $.Class({
 		if (this.mutable) {
 			// Insert the add button if it's not already in the DOM
 			if (!this.addButton.parentNode) {
-				if (this.bottomUp) {
-					this.addButton._.before($.value(this.children[0], "element") || this.marker);
-				}
-				else {
-					var tag = this.element.tagName.toLowerCase();
-					var containerSelector = Mavo.selectors.container[tag];
-
-					if (containerSelector) {
-						var after = this.marker.parentNode.closest(containerSelector);
-					}
-
-					this.addButton._.after(after && after.parentNode? after : this.marker);
-				}
+				var tag = this.element.tagName.toLowerCase();
+				var containerSelector = Mavo.selectors.container[tag];
+				var rel = containerSelector? this.marker.parentNode.closest(containerSelector) : this.marker;
+				$[this.bottomUp? "before" : "after"](this.addButton, rel);
 			}
 
 			// Set up drag & drop
@@ -3331,7 +3320,7 @@ var _ = Mavo.Collection = $.Class({
 				Mavo.hooks.run("collection-add-end", env);
 			});
 
-			this.marker.parentNode.insertBefore(fragment, this.marker);
+			$[this.bottomUp? "after" : "before"](fragment, this.marker);
 		}
 	},
 
@@ -4081,6 +4070,7 @@ var _ = Mavo.ExpressionText = $.Class({
 			this.primitive.value = ret;
 		}
 		else {
+			ret = ret.presentational || ret;
 			Mavo.Primitive.setValue(this.node, ret, {attribute: this.attribute});
 		}
 
@@ -4197,7 +4187,7 @@ var _ = Mavo.Expressions = $.Class({
 		rootGroup.walk((obj, path) => {
 			if (obj.expressions && obj.expressions.length && !obj.isDeleted()) {
 				let env = { context: this, data: $.value(data, ...path) };
-
+// if (evt && evt.action == "delete") console.log(data, path, env.data, obj.element);
 				Mavo.hooks.run("expressions-update-start", env);
 
 				for (let et of obj.expressions) {

src/collection.js
@@ -111,9 +111,7 @@ var _ = Mavo.Collection = $.Class({
 
 		if (this.mutable) {
 			// Add it to the DOM, or fix its place
-			var nextItem = this.children[index];
-
-			item.element._.before(nextItem && nextItem.element || this.marker);
+			$[this.bottomUp? "after" : "before"](item.element, this.marker);
 		}
 
 		var env = {context: this, item};
@@ -234,19 +232,10 @@ var _ = Mavo.Collection = $.Class({
 		if (this.mutable) {
 			// Insert the add button if it's not already in the DOM
 			if (!this.addButton.parentNode) {
-				if (this.bottomUp) {
-					this.addButton._.before($.value(this.children[0], "element") || this.marker);
-				}
-				else {
-					var tag = this.element.tagName.toLowerCase();
-					var containerSelector = Mavo.selectors.container[tag];
-
-					if (containerSelector) {
-						var after = this.marker.parentNode.closest(containerSelector);
-					}
-
-					this.addButton._.after(after && after.parentNode? after : this.marker);
-				}
+				var tag = this.element.tagName.toLowerCase();
+				var containerSelector = Mavo.selectors.container[tag];
+				var rel = containerSelector? this.marker.parentNode.closest(containerSelector) : this.marker;
+				$[this.bottomUp? "before" : "after"](this.addButton, rel);
 			}
 
 			// Set up drag & drop
@@ -361,7 +350,7 @@ var _ = Mavo.Collection = $.Class({
 				Mavo.hooks.run("collection-add-end", env);
 			});
 
-			this.marker.parentNode.insertBefore(fragment, this.marker);
+			$[this.bottomUp? "after" : "before"](fragment, this.marker);
 		}
 	},
 

src/expressions.js
@@ -69,7 +69,7 @@ var _ = Mavo.Expressions = $.Class({
 		rootGroup.walk((obj, path) => {
 			if (obj.expressions && obj.expressions.length && !obj.isDeleted()) {
 				let env = { context: this, data: $.value(data, ...path) };
-
+// if (evt && evt.action == "delete") console.log(data, path, env.data, obj.element);
 				Mavo.hooks.run("expressions-update-start", env);
 
 				for (let et of obj.expressions) {

src/expressiontext.js
@@ -148,6 +148,7 @@ var _ = Mavo.ExpressionText = $.Class({
 			this.primitive.value = ret;
 		}
 		else {
+			ret = ret.presentational || ret;
 			Mavo.Primitive.setValue(this.node, ret, {attribute: this.attribute});
 		}
 

commit 23c45e85bb2d4210be4543fed56d9e09db2999a7

Fix bug with inline add button
Index was ignored due to changes that
848298e5707e6472966e1cd38b7a094c739bba49 introduced

dist/mavo.es5.js
@@ -3462,13 +3462,15 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 				this.adopt(item);
 			}
 
-			if (index === undefined) {
-				index = this.bottomUp ? 0 : this.length;
-			}
-
 			if (this.mutable) {
 				// Add it to the DOM, or fix its place
-				$[this.bottomUp ? "after" : "before"](item.element, this.marker);
+				if (index === undefined) {
+					$[this.bottomUp ? "after" : "before"](item.element, this.marker);
+					index = this.bottomUp ? 0 : this.length;
+				} else {
+					var rel = this.children[index];
+					$[this.bottomUp ? "after" : "before"](item.element, rel.element);
+				}
 			}
 
 			var env = { context: this, item: item };

dist/mavo.js
@@ -3075,13 +3075,16 @@ var _ = Mavo.Collection = $.Class({
 			this.adopt(item);
 		}
 
-		if (index === undefined) {
-			index = this.bottomUp? 0 : this.length;
-		}
-
 		if (this.mutable) {
 			// Add it to the DOM, or fix its place
-			$[this.bottomUp? "after" : "before"](item.element, this.marker);
+			if (index === undefined) {
+				$[this.bottomUp? "after" : "before"](item.element, this.marker);
+				index = this.bottomUp? 0 : this.length;
+			}
+			else {
+				var rel = this.children[index];
+				$[this.bottomUp? "after" : "before"](item.element, rel.element);
+			}
 		}
 
 		var env = {context: this, item};

src/collection.js
@@ -105,13 +105,16 @@ var _ = Mavo.Collection = $.Class({
 			this.adopt(item);
 		}
 
-		if (index === undefined) {
-			index = this.bottomUp? 0 : this.length;
-		}
-
 		if (this.mutable) {
 			// Add it to the DOM, or fix its place
-			$[this.bottomUp? "after" : "before"](item.element, this.marker);
+			if (index === undefined) {
+				$[this.bottomUp? "after" : "before"](item.element, this.marker);
+				index = this.bottomUp? 0 : this.length;
+			}
+			else {
+				var rel = this.children[index];
+				$[this.bottomUp? "after" : "before"](item.element, rel.element);
+			}
 		}
 
 		var env = {context: this, item};