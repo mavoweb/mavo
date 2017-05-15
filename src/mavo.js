5c4ee2d87abca1aa53dfb241ae905bd3278c667f
Lea Verou <lea@verou.me>
2017-05-15 01:51:30 -0400

    Fix Mavo in Chrome <= 57

--- src/mavo.js
+++ src/mavo.js
@@ -708,7 +708,7 @@ requestAnimationFrame(() => {
 	_.dependencies.push(
 		$.ready(),
 		_.Plugins.load(),
-		$.include(!polyfills.length, `https://cdn.polyfill.io/v2/polyfill.min.js?features=${polyfills.join(",")}`),
+		$.include(!polyfills.length, "https://cdn.polyfill.io/v2/polyfill.min.js?features=" + polyfills.join(","))
 	);
 
 	$.ready().then(() => {
