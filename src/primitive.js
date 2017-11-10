mavoweb/mavo

commit 6170dc69d86c5119858dfd54554e03309f326aa3

Fix #295

src/primitive.js
@@ -459,7 +459,7 @@ var _ = Mavo.Primitive = $.Class({
 
 		if (data === undefined) {
 			// New property has been added to the schema and nobody has saved since
-			if (!this.modes) {
+			if (this.modes !== "read") {
 				this.value = this.closestCollection? this.default : this.templateValue;
 			}
 		}
@@ -531,7 +531,7 @@ var _ = Mavo.Primitive = $.Class({
 				this.editorValue = value;
 			}
 
-			if (!this.editing || this.popup || !this.editor) {
+			if (this.popup || !this.editor || this.editor !== document.activeElement) { // Prevent loops
 				if (this.config.setValue) {
 					this.config.setValue.call(this, this.element, value);
 				}
@@ -568,7 +568,6 @@ var _ = Mavo.Primitive = $.Class({
 	live: {
 		default: function (value) {
 			if (this.value == this._default) {
-
 				this.value = value;
 			}
 		},
@@ -721,7 +720,7 @@ var _ = Mavo.Primitive = $.Class({
 				o.datatype = o.datatype !== undefined? o.datatype : o.config.datatype;
 
 				if (o.config.setValue && o.attribute == o.config.attribute) {
-					return o.config.setValue(element, value);
+					return o.config.setValue(element, value, o.attribute);
 				}
 			}
 

src/elements.js
@@ -31,7 +31,7 @@ Object.defineProperties(_, {
 			if (config.extend) {
 				var base = _[config.extend];
 
-				config = $.extend($.extend({}, base, p => p != "selector"), config);
+				config = $.extend($.extend({}, base), config);
 			}
 
 			if (id.indexOf("@") > -1) {
@@ -56,7 +56,6 @@ Object.defineProperties(_, {
 				}
 			}
 			else {
-
 				_[id] = config;
 			}
 
@@ -302,6 +301,7 @@ _.register({
 
 	"textarea": {
 		extend: "formControl",
+		selector: "textarea",
 		attribute: null,
 		getValue: element => element.value,
 		setValue: (element, value) => element.value = value
@@ -310,7 +310,32 @@ _.register({
 	"formNumber": {
 		extend: "formControl",
 		selector: "input[type=range], input[type=number]",
-		datatype: "number"
+		datatype: "number",
+		setValue: function(element, value) {
+			element.value = value;
+			element.setAttribute("value", value);
+
+			var attribute = value > element.value? "max" : "min";
+
+			if (!isNaN(value) && element.value != value && !Mavo.data(element, "boundObserver")) {
+				// Value out of bounds, maybe race condition? See #295
+				// Observe min/max attrs until user interaction or data change
+				var observer = new Mavo.Observer(element, attribute, r => {
+					element.value = value;
+				});
+
+				requestAnimationFrame(() => {
+					$.events(element, "input mavo:datachange", function handler() {
+						observer.destroy();
+						Mavo.data(element, "boundObserver", undefined);
+						$.unbind(element, "input mavo:datachange", handler);
+					});
+				});
+
+				// Prevent creating same observer twice
+				Mavo.data(element, "boundObserver", observer);
+			}
+		}
 	},
 
 	"checkbox": {
@@ -321,8 +346,9 @@ _.register({
 		changeEvents: "click"
 	},
 
-	"input[type=radio]": {
+	"radio": {
 		extend: "formControl",
+		selector: "input[type=radio]",
 		attribute: "checked",
 		modes: "edit",
 		getValue: element => {

commit 4afba4bd9579792d710f57d5f510535f1765035b

Fix regression caused after 6170dc69d86c5119858dfd54554e03309f326aa3

src/primitive.js
@@ -461,7 +461,7 @@ var _ = Mavo.Primitive = $.Class({
 
 		if (data === undefined) {
 			// New property has been added to the schema and nobody has saved since
-			if (this.modes !== "read") {
+			if (!this.modes) {
 				this.value = this.closestCollection? this.default : this.templateValue;
 			}
 		}