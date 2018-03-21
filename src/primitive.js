7cd9cbe78190a355b16b272597ccc0233e445737
Lea Verou <lea@verou.me>
2018-03-21 14:27:46 -0400

    Fixes

--- src/primitive.js
+++ src/primitive.js
@@ -730,7 +730,9 @@ var _ = Mavo.Primitive = $.Class({
 				attribute = null;
 			}
 
-			if (!datatype && attribute == _.getValueAttribute(element)) {
+			isAttributeDefault = attribute === undefined || attribute == _.getValueAttribute(element);
+
+			if (!datatype && isAttributeDefault) {
 				datatype = element.getAttribute("datatype") || undefined;
 			}
 
