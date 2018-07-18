5423ce1f07671cda55a05f823044cc50fe1ff6e1
Kenneth Chen <efeichen@gmail.com>
2018-07-18 13:09:35 -0400

    Revert "Add permission, apiDomain, and oAuth endpoints"
    This reverts commit 9f7222bb57249bac16eab575120927af2cc0d8a5.

--- src/backend.gdrive.js
+++ src/backend.gdrive.js
@@ -12,7 +12,7 @@ var _ = Mavo.Backend.register($.Class({
         this.url; // URL object from this.source
         this.mavo; // Mavo instance
         this.format; // Current format
-        this.permissions.on(["login", "read"]); // Permissions of this particular backend.
+        this.permissions; // Permissions of this particular backend.
     },
 
     // Low-level functions for reading data. You don’t need to implement this
@@ -65,11 +65,9 @@ var _ = Mavo.Backend.register($.Class({
     },
 
     static: {
-        apiDomain: "https://www.googleapis.com",
-        oAuth: "https://accounts.google.com/o/oauth2/v2/auth",
         // Mandatory and very important! This determines when your backend is used.
         // value: The mv-storage/mv-source/mv-init value
-        test: function(url) {
+        test: function(value) {
             // Returns true if this value applies to this backend
         }
     }
