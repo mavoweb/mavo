7aebdf378b253466cfffcb46d17fbf8a49317212
hopeful2 <hopeful2@goandmake.app>
2019-01-24 07:07:52 +1100

    revised style on domexpression check before unregister fixes #436

--- src/expressions.js
+++ src/expressions.js
@@ -48,7 +48,7 @@ var _ = Mavo.Expressions = $.Class({
 			}
 			// just in case domexpresssion has been destroyed by another app during the loop
 			// when another app is destroyed.
-			if (id in Mavo.all && 'undefined' !== typeof domexpresssion) {
+			if (id in Mavo.all && typeof domexpresssion !== "undefined") {
 				// Cross-mavo expressions
 				Mavo.all[id].expressions.unregister(domexpresssion);
 			}
