cd5e9cb7daaa3194ca9b2144d38348e401041a4d
Lea Verou <lea@verou.me>
2020-06-15 14:15:32 -0400

    Convert Mavo.Expression and Mavo.Expression.Syntax to ES6 classes

--- src/expression.js
+++ src/expression.js
@@ -1,11 +1,11 @@
-(function($, $$) {
+(function() {
 
-var _ = Mavo.Expression = $.Class({
-	constructor: function(expression) {
+var _ = Mavo.Expression = class Expression {
+	constructor (expression) {
 		this.expression = expression;
-	},
+	}
 
-	eval: function(data = Mavo.Data.stub, o) {
+	eval (data = Mavo.Data.stub, o) {
 		Mavo.hooks.run("expression-eval-beforeeval", this);
 
 		if (!this.function) {
@@ -43,31 +43,33 @@ var _ = Mavo.Expression = $.Class({
 
 			return error;
 		}
-	},
+	}
 
-	error(title, ...message) {
+	error (title, ...message) {
 		message = message.join("\n");
 		console.info(`%cOops! 😳 ${title}:`, "color: #c04; font-weight: bold;", message);
-	},
+	}
 
-	toString() {
+	toString () {
 		return this.expression;
-	},
+	}
 
-	changedBy: function(evt) {
+	changedBy (evt) {
 		return _.changedBy(this.identifiers, evt);
-	},
+	}
+};
 
+$.Class(_, {
 	live: {
 		expression: function(value) {
 			this.function = null;
 			this.identifiers = value.match(/[$a-z][$\w]*/ig) || [];
 		}
-	},
+	}
 });
 
-_.Syntax = $.Class({
-	constructor: function(start, end) {
+_.Syntax = class Syntax {
+	constructor (start, end) {
 		this.start = start;
 		this.end = end;
 		// Try to parse anything between start and end as an expression. Note
@@ -79,15 +81,15 @@ _.Syntax = $.Class({
 		// Regex note: "[\S\s]" matches all characters, unlike ".", which
 		// doesn't match newlines.
 		this.regex = RegExp(`${Mavo.escapeRegExp(start)}([\\S\\s]*?)${Mavo.escapeRegExp(end)}`, "gi");
-	},
+	}
 
-	test: function(str) {
+	test (str) {
 		this.regex.lastIndex = 0;
 
 		return this.regex.test(str);
-	},
+	}
 
-	tokenize: function(str) {
+	tokenize (str) {
 		var match, ret = [], lastIndex = 0;
 
 		this.regex.lastIndex = 0;
@@ -116,24 +118,21 @@ _.Syntax = $.Class({
 		}
 
 		return ret;
-	},
-
-	static: {
-		create: function(element) {
-			if (element) {
-				var syntax = element.getAttribute("mv-expressions");
-
-				if (syntax) {
-					syntax = syntax.trim();
-					return /\s/.test(syntax)? new _.Syntax(...syntax.split(/\s+/)) : _.Syntax.ESCAPE;
-				}
-			}
-		},
-
-		ESCAPE: -1
 	}
-});
 
+	static create (element) {
+		if (element) {
+			var syntax = element.getAttribute("mv-expressions");
+
+			if (syntax) {
+				syntax = syntax.trim();
+				return /\s/.test(syntax)? new _.Syntax(...syntax.split(/\s+/)) : _.Syntax.ESCAPE;
+			}
+		}
+	}
+};
+
+_.Syntax.ESCAPE = -1;
 _.Syntax.default = new _.Syntax("[", "]");
 
-})(Bliss, Bliss.$);
+})();
