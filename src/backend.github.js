25f37487e55219c6b02bb0e80a0f4dd7a11f29ad
Lea Verou <lea@verou.me>
2021-10-18 20:57:33 +0300

    Experimental implementation of fetching multiple pages for Github API calls, fixes #781
    Eventually we'd want this to move to the base Backend class, if we can generalize it sufficiently

--- src/backend.github.js
+++ src/backend.github.js
@@ -64,18 +64,67 @@ let _ = Mavo.Backend.register(class Github extends Mavo.Backend {
 					});
 			}
 
-			let response = await this.request(info.apiCall, {ref:this.branch}, "GET", {
+			let isRawAPICall = !!info.apiParams;
+			let responseType = isRawAPICall ? "response" : "json";
+			let req = {
+				responseType,
 				headers: {
 					"Accept": "application/vnd.github.squirrel-girl-preview"
 				}
-			});
+			};
+			let response = await this.request(info.apiCall, {ref:this.branch}, "GET", req);
 
-			if (info.repo && response.content) {
-				// Fetching file contents
-				return _.atob(response.content);
+			if (isRawAPICall) {
+				// Raw API call
+				let json = await response.json();
+
+				let params = new URL(info.apiCall, this.constructor.apiDomain).searchParams;
+				let maxPages = params.get("max_pages") - 1; /* subtract 1 because we already fetched a page */
+
+				if (maxPages > 0 && params.get("page") === null && Array.isArray(json)) {
+					// Fetch more pages
+					let next;
+
+					do {
+						next = response.headers.get("Link")?.match(/<(.+?)>; rel="next"/)?.[1];
+						console.log(next);
+
+						if (next) {
+							response = await this.request(next, {ref:this.branch}, "GET", req);
+
+							if (response.ok) {
+								let pageJSON = await response.json();
+
+								if (Array.isArray(pageJSON)) {
+									json.push(...pageJSON);
+								}
+								else {
+									break;
+								}
+							}
+							else {
+								break;
+							}
+						}
+						else {
+							break;
+						}
+
+					} while (--maxPages > 0);
+
+				}
+
+				return json;
+
 			}
 			else {
-				return response;
+				if (info.repo && response.content) {
+					// Fetching file contents
+					return _.atob(response.content);
+				}
+				else {
+					return response;
+				}
 			}
 		}
 		else {
