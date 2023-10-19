f54d0ff127fa52794f4dd7ad4d1b39b0e71b3070
Akshat Sharma <111536616+Akshat111111@users.noreply.github.com>
2023-10-19 19:48:14 +0530

    Update ui.popup.js
    Simplified DOM manipulation using element.appendChild() and element.remove(),Added Arrow function,Removed unnecessary variables and added ES6 classes.

--- src/ui.popup.js
+++ src/ui.popup.js
@@ -1,140 +1,113 @@
-(function($, $$) {
+class Popup {
+  constructor(primitive) {
+    this.primitive = primitive;
+    this.shown = false;
+    this.editor = primitive.editor;
+    this.element = document.createElement("div");
+    this.element.classList.add("mv-popup");
+    this.element.hidden = true;
 
-var _ = Mavo.UI.Popup = $.Class({
-	constructor: function(primitive) {
-		this.primitive = primitive;
+    this.position = () => {
+      const bounds = this.primitive.element.getBoundingClientRect();
+      const x = bounds.left;
+      let y = bounds.bottom;
+      let pointDown = false;
 
-		// Need to be defined here so that this is what expected
-		this.position = evt => {
-			var bounds = this.primitive.element.getBoundingClientRect();
-			var x = bounds.left;
-			var y = bounds.bottom;
-			var pointDown = false;
+      if (this.element.offsetHeight) {
+        this.height = this.element.getBoundingClientRect().height || this.height;
+      }
 
-			if (this.element.offsetHeight) {
-				// Is in the DOM, check if it fits
-				this.height = this.element.getBoundingClientRect().height || this.height;
-			}
+      if (this.height + y + 20 > window.innerHeight) {
+        if (bounds.top - this.height > 20) {
+          pointDown = true;
+          y = bounds.top - this.height - 20;
+        } else {
+          y = window.innerHeight - this.height - 20;
+        }
+      }
 
-			if (this.height + y + 20 > innerHeight) {
-				// Normal positioning means the popup would be cut off or too close to the edge, adjust
+      this.element.classList.toggle("mv-point-down", pointDown);
+      this.element.style.top = `${y}px`;
+      this.element.style.left = `${x}px`;
+    };
 
-				// Perhaps placing it above is better
-				if (bounds.top - this.height > 20) {
-					var pointDown = true;
-					y = bounds.top - this.height - 20;
-				}
-				else {
-					// Nah, just raise it a bit
-					y = innerHeight - this.height - 20;
-				}
-			}
+    this.element.innerHTML = `
+      <fieldset>
+        <legend>${this.primitive.label}:</legend>
+        ${this.editor.outerHTML}
+      </fieldset>
+    `;
 
-			this.element.classList.toggle("mv-point-down", pointDown);
+    this.element.addEventListener("keyup", (evt) => {
+      if (evt.keyCode === 13 || evt.keyCode === 27) {
+        if (this.element.contains(document.activeElement)) {
+          this.primitive.element.focus();
+        }
+        evt.stopPropagation();
+        this.hide();
+      }
+    });
 
-			$.style(this.element, { top:  `${y}px`, left: `${x}px` });
-		};
+    this.element.addEventListener("transitionend", this.position);
+    this.hideCallback = (evt) => {
+      if (!this.element.contains(evt.target) && !this.primitive.element.contains(evt.target)) {
+        this.hide();
+      }
+    };
 
-		this.element = $.create("div", {
-			className: "mv-popup",
-			hidden: true,
-			contents: {
-				tag: "fieldset",
-				contents: [
-					{
-						tag: "legend",
-						textContent: this.primitive.label + ":"
-					},
-					this.editor
-				]
-			},
-			events: {
-				keyup: evt => {
-					if (evt.keyCode == 13 || evt.keyCode == 27) {
-						if (this.element.contains(document.activeElement)) {
-							this.primitive.element.focus();
-						}
+    if (this.editor.matches("select")) {
+      this.editor.size = Math.min(10, this.editor.children.length);
+    }
+  }
 
-						evt.stopPropagation();
-						this.hide();
-					}
-				},
-				transitionend: this.position
-			}
-		});
+  show() {
+    this.shown = true;
+    this.element.style.transition = "none";
+    this.element.removeAttribute("hidden");
+    this.position();
+    this.element.setAttribute("hidden", "");
+    this.element.style.transition = "";
+    document.body.appendChild(this.element);
 
-		// No point in having a dropdown in a popup
-		if (this.editor.matches("select")) {
-			this.editor.size = Math.min(10, this.editor.children.length);
-		}
-		this.hideCallback = evt => {
-			if (!this.element.contains(evt.target) && !this.primitive.element.contains(evt.target)) {
-				this.hide();
-			}
-		};
-	},
+    setTimeout(() => {
+      this.element.removeAttribute("hidden");
+    }, 100);
 
-	show: function() {
-		$.unbind([this.primitive.element, this.element], ".mavo:showpopup");
+    document.addEventListener("focus", this.hideCallback, true);
+    window.addEventListener("scroll", this.position, { passive: true });
+  }
 
-		this.shown = true;
+  hide() {
+    document.removeEventListener("focus", this.hideCallback, true);
+    window.removeEventListener("scroll", this.position, { passive: true });
+    this.element.setAttribute("hidden", "");
+    this.shown = false;
 
-		this.element.style.transition = "none";
-		this.element.removeAttribute("hidden");
+    setTimeout(() => {
+      this.element.remove();
+    }, parseFloat(getComputedStyle(this.element).transitionDuration) * 1000 || 400);
+  }
 
-		this.position();
+  prepare() {
+    this.primitive.element.addEventListener("click", (evt) => {
+      this.show();
+    });
 
-		this.element.setAttribute("hidden", "");
-		this.element.style.transition = "";
+    this.primitive.element.addEventListener("keyup", (evt) => {
+      if ([13, 113].includes(evt.keyCode)) { // Enter or F2
+        this.show();
+        this.editor.focus();
+      }
+    });
 
-		document.body.appendChild(this.element);
+    if (!this.element.contains(this.editor)) {
+      this.element.appendChild(this.editor);
+    }
+  }
 
-		setTimeout(() => {
-			this.element.removeAttribute("hidden");
-		}, 100); // trigger transition. rAF or timeouts < 100 don't seem to, oddly.
+  close() {
+    this.hide();
+    this.primitive.element.removeEventListener("click", this.show);
+  }
+}
 
-		$.bind(document, "focus click", this.hideCallback, true);
-		window.addEventListener("scroll", this.position, {passive: true});
-	},
-
-	hide: function() {
-		$.unbind(document, "focus click", this.hideCallback, true);
-		window.removeEventListener("scroll", this.position, {passive: true});
-		this.element.setAttribute("hidden", ""); // trigger transition
-		this.shown = false;
-
-		setTimeout(() => {
-			$.remove(this.element);
-		}, parseFloat(getComputedStyle(this.element).transitionDuration) * 1000 || 400); // TODO transition-duration could override this
-	},
-
-	prepare: function() {
-		$.bind(this.primitive.element, {
-			"click.mavo:edit": evt => {
-				this.show();
-			},
-			"keyup.mavo:edit": evt => {
-				if ([13, 113].indexOf(evt.keyCode) > -1) { // Enter or F2
-					this.show();
-					this.editor.focus();
-				}
-			}
-		});
-
-		if (!this.element.contains(this.editor)) {
-			// This can happen if edit type changes from popup to inline
-			this.element.append(this.editor);
-		}
-	},
-
-	close: function() {
-		this.hide();
-		$.unbind(this.primitive.element, ".mavo:edit .mavo:preedit .mavo:showpopup");
-	},
-
-	proxy: {
-		"editor": "primitive"
-	}
-});
-
-})(Bliss, Bliss.$);
